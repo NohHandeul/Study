0bed066b2e689c80936a413c09185fb641d6336f
NohHanDeul <roh950714@gmail.com>
2022-04-12 22:23:33 +0900

    weekend3Commit

---
+++ weekend2/js/javascript.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const stringdata = 'Compressing objects: 100% (7/7), done'
++'Writing objects: 100% (9/9), 845 bytes | 211.00 KiB/s, done'
++'asdfasdfasdfasf'
+
+const stringdata2 = `Compressing objects: 100% (7/7), done.
+Writing objects: 100% (9/9), 845 bytes | 211.00 KiB/s, done.
+Total 9 (delta 3), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (3/3), completed with 2 local objects.
+To https://github.com/NohHandeul/study.git
+   fb2953f..20d78cd  main -> main
+PS D:\studyproject\study> 
+`
